59ce3954ac7491029e7d12b176fa75b973e82f9f
chalmejd-TEAM <151769819+chalmejd-TEAM@users.noreply.github.com>
2025-01-22 08:19:43 -0600

    Prompt user for file and added time data for each channel

--- main.m
+++ main.m
@@ -1,8 +1,24 @@
-data = S46_PPA_dw_data_reader('Add .dxd file path here');
+clc
+clear
+close all
 
+% Prompting user for target Dewesoft file
+[file,location] = uigetfile('*.dxd');
+fullFileName = fullfile(location, file);
 
+% Importing data from Dewesoft file
+data = dw_data_reader(fullFileName);
+
+% Loop through each channel in the Dewesoft file
 for i = 1:height(data)
+    % Remove invalid characters from channel names for variables
     channel = regexprep(cell2mat(data.name(i)), '[\% ()#/\\-]', '');
+    % Change cell array to matlab array
     output = cell2mat(data.data(i));
-    eval(['Dewe_' channel '= output;']);
+    % Assign output array to variable matching channel name
+    eval([channel '= output;']);
+    
+    % Repeat above for time data for each channel
+    timeOutput = cell2mat(data.time(i));
+    eval([channel '_time = timeOutput;']);
 end
